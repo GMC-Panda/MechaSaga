9b687480739788e68ea8bd4ce5ea70ae5dbca594
Panda <misazurame@web.de>
2020-12-28 20:59:13 +0100

    Removed the written dialogue editor system and replaced it
    Replaced it with Dialogue System For Unity

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewQuest", menuName = "Questsystem/NewQuest", order = 0)]
+public class Quest : ScriptableObject 
+{
+    [SerializeField] string[] tasks;    
+}
